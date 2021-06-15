6ff5198c2a6263a99c85b583af0d42da543d37db
Michael Van Der Poll <michaelvdp@gmail.com>
2021-06-15 21:24:23 +0200

    Adds door script to load next level

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
