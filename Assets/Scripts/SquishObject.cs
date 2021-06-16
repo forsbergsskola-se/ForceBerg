24e56ad7fd7a7098a51e346628a78d6d4ebc2126
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-06-16 21:20:20 +0200

    Adds squish sfx to gravity door

--- Assets/Scripts/SquishObject.cs
+++ Assets/Scripts/SquishObject.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SquishObject : MonoBehaviour
 {
     public int squishAmount = 10;
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.TryGetComponent<PlayerHealth>(out var health))
@@ -10,10 +11,12 @@ public class SquishObject : MonoBehaviour
             var rb2d = GetComponent<Rigidbody2D>();
             var hitAmount = rb2d.velocity.magnitude;
             var playerVelocity = other.rigidbody.velocity.magnitude;
+            var squishSfx = GetComponent<AudioSource>();
             Debug.Log(hitAmount - playerVelocity);
             if (hitAmount < playerVelocity)
                 return;
             health.TakeDamage(squishAmount);
+            squishSfx.Play();
             Debug.Log(hitAmount);
         }
     }
