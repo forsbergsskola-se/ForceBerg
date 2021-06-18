66556a0e31a5cab9fd8884d0f84abc831f6a358a
Exoduz85 <47660094+Exoduz85@users.noreply.github.com>
2021-06-18 13:42:45 +0200

    update to echo effect
    Echo effect now works with can being crushed!

--- Assets/Scripts/EchoEffect.cs
+++ Assets/Scripts/EchoEffect.cs
@@ -5,10 +5,12 @@ public class EchoEffect : MonoBehaviour
     private float timeBtwSpawns;
     [SerializeField] private float startTimeBtwSpawns;
     [SerializeField] private GameObject echo;
+    [SerializeField] private GameObject playerModelGO;
     void Update() {
-        if (!(this.GetComponent<Rigidbody2D>().velocity.magnitude > 0.1f)) return;
+        if (!(this.GetComponent<Rigidbody2D>().velocity.magnitude > 1f)) return;
         if (timeBtwSpawns <= 0) {
             GameObject instance = Instantiate(echo, this.transform.position, quaternion.identity);
+            instance.transform.localScale = playerModelGO.transform.localScale;
             Destroy(instance, 0.8f);
             timeBtwSpawns = startTimeBtwSpawns;
         } else {
